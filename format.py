62f3644b523b6520583f78c5a328b43a972241b2
Toddelismyname <116207910+Toddelismyname@users.noreply.github.com>
2025-02-15 12:55:07 +0100

    Removed the rbf_subtag_elements array logic as it is not used

--- format.py
+++ format.py
@@ -185,7 +185,6 @@ class PrettyPrinter():
                 mesh_elements = []
                 data_elements = []
                 mapping_elements = []
-                rbf_subtag_elements = []
                 
                 for child in sorted_participant_children:
                     if str(child.tag) in ['provide-mesh', 'receive-mesh']:
@@ -194,9 +193,6 @@ class PrettyPrinter():
                         data_elements.append(child)
                     elif str(child.tag).startswith('mapping:'):
                         mapping_elements.append(child)
-                        if str(child.tag).startswith('mapping:rbf'):
-                            if len(child) > 0 and str(child[0].tag).startswith('basis-function:'):
-                                rbf_subtag_elements.append(child)
                 
                 # Construct participant tag with attributes
                 participant_tag = "<{}".format(group.tag)
