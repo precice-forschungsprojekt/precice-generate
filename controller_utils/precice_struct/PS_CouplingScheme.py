b4246cf6190d8d613d42ef589b46a6c16432a234
Toddelismyname <116207910+Toddelismyname@users.noreply.github.com>
2025-02-21 13:41:09 +0100

    Revert "idea use userinput from to and just paste"
    This reverts commit 9b208565fc3ddc6e6c72da41c2b8110f8214f17a.

--- controller_utils/precice_struct/PS_CouplingScheme.py
+++ controller_utils/precice_struct/PS_CouplingScheme.py
@@ -139,19 +139,27 @@ class PS_CouplingScheme(object):
                         break
 
             # the from and to attributes
-            from_s = q.exchange.get('from')
-            to_s = q.exchange.get('to')
+            from_s = "___"
+            to_s = "__"
             exchange_mesh_name = q.source_mesh_name
             
             if coupled_mesh_name:
                 if solver.name != simple_solver.name:
+                    from_s = solver.name
+                    to_s = simple_solver.name
                     exchange_mesh_name = coupled_mesh_name
                 else:
+                    from_s = solver.name
+                    to_s = other_solver_for_coupling.name
                     exchange_mesh_name = coupled_mesh_name or q.source_mesh_name
             else:
                 if solver.name != simple_solver.name:
+                    from_s = solver.name
+                    to_s = simple_solver.name
                     exchange_mesh_name = other_mesh_name
                 else:
+                    from_s = solver.name
+                    to_s = other_solver_for_coupling.name
                     exchange_mesh_name = q.source_mesh_name
 
             e = etree.SubElement(coupling_scheme, "exchange", data=q_name, mesh=exchange_mesh_name
