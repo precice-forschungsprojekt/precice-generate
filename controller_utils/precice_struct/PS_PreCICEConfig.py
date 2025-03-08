5167ffc52ed3e0a26dd320e872831bc0eb8a2d8f
Toddelismyname <116207910+Toddelismyname@users.noreply.github.com>
2025-03-08 12:06:28 +0100

    dont copy because none cant be copied

--- controller_utils/precice_struct/PS_PreCICEConfig.py
+++ controller_utils/precice_struct/PS_PreCICEConfig.py
@@ -102,7 +102,7 @@ class PS_PreCICEConfig(object):
         """Creates the main preCICE config from the UI structure."""
 
         self.exchanges = user_input.exchanges.copy()
-        self.acceleration = user_input.acceleration.copy()
+        self.acceleration = user_input.acceleration
 
         # participants
         for participant_name in user_input.participants:
