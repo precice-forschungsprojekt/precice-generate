617bb0910273d57279277752811135341574c674
Toddelismyname <116207910+Toddelismyname@users.noreply.github.com>
2025-03-17 18:06:13 +0100

    differentiate between different accelerations

--- controller_utils/precice_struct/PS_CouplingScheme.py
+++ controller_utils/precice_struct/PS_CouplingScheme.py
@@ -284,18 +284,19 @@ class PS_ImplicitPostProcessing(object):
         if acceleration is not None:
             for a, b in acceleration.items():
                 if b is not None:
-                    if a == "initial-relaxation" or a == "max-used-iterations" or a == "time-windows-reused":
-                        i = etree.SubElement(post_processing, a, value=str(b))
-                    elif a == "filter":
-                        if b.get("type") is not None:
-                            i = etree.SubElement(post_processing, a, limit=str(b.get("limit")), type=str(b.get("type")))
-                        else:
-                            i = etree.SubElement(post_processing, a, limit=str(b.get("limit")))
-                    elif a == "preconditioner":
-                        if b.get("type") is not None:
-                            i = etree.SubElement(post_processing, a, freeze_after=str(b.get("freeze-after")), type=str(b.get("type")))
-                        else:
-                            i = etree.SubElement(post_processing, a, freeze_after=str(b.get("freeze-after")))
+                    if self.name == "IQN-ILS":
+                        if a == "initial-relaxation" or a == "max-used-iterations" or a == "time-windows-reused":
+                            i = etree.SubElement(post_processing, a, value=str(b))
+                        elif a == "filter":
+                            if b.get("type") is not None:
+                                i = etree.SubElement(post_processing, a, limit=str(b.get("limit")), type=str(b.get("type")))
+                            else:
+                                i = etree.SubElement(post_processing, a, limit=str(b.get("limit")))
+                        elif a == "preconditioner":
+                            if b.get("type") is not None:
+                                i = etree.SubElement(post_processing, a, freeze_after=str(b.get("freeze-after")), type=str(b.get("type")))
+                            else:
+                                i = etree.SubElement(post_processing, a, freeze_after=str(b.get("freeze-after")))
         
         if simple_solver:
             for q_name, q in config.coupling_quantities.items():
