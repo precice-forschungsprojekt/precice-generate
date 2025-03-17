feb9fa8096fa00250e6d86a7909546d7ae8d1e84
Toddelismyname <116207910+Toddelismyname@users.noreply.github.com>
2025-03-17 13:08:09 +0100

    Revert "Add optional mesh argument"
    This reverts commit 60e861df9ed6b8b7f27d05dea4f76de3cd7ef5d6.

--- controller_utils/ui_struct/UI_UserInput.py
+++ controller_utils/ui_struct/UI_UserInput.py
@@ -128,7 +128,6 @@ class UI_UserInput(object):
                 first_ex = ex_list[0]
                 coupling.boundaryC1 = first_ex.get("from-patch", "")
                 coupling.boundaryC2 = first_ex.get("to-patch", "")
-                coupling.mesh = first_ex.get("mesh", "")
 
                 self.couplings.append(coupling)
                 coupling.partitcipant1.list_of_couplings.append(coupling)
