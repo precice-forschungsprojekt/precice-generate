607fb539d9779046d8b35750d8284fd173f297b6
Toddelismyname <116207910+Toddelismyname@users.noreply.github.com>
2025-04-17 11:02:06 +0200

    try using new operations

--- controller_utils/precice_struct/PS_PreCICEConfig.py
+++ controller_utils/precice_struct/PS_PreCICEConfig.py
@@ -395,6 +395,7 @@ class PS_PreCICEConfig(object):
         # 4 coupling scheme
         # TODO: later this migh be more complex !!!
         self.couplingScheme.write_precice_xml_config(precice_configuration_tag, self)
+        pass
 
         # =========== generate XML ===========================
 
