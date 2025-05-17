6f020fcada86924fd9b5be284fb0cd55eac2cba4
Toddelismyname <116207910+Toddelismyname@users.noreply.github.com>
2025-05-17 15:59:36 +0200

    v17

--- src/precice_config.py
+++ src/precice_config.py
@@ -1,42 +1,20 @@
+from topology_input import TopologyInput
+import xml.etree.ElementTree as etree
+
 class PreciceConfig:
     """Class to represent the preCICE configuration."""
 
-    # def __init__(self, participant: str, mesh_id: str, data_id: str, coupling_scheme: str):
-    #     """
-    #     Constructor to initialize the preCICE configuration.
+    def __init__(self):
+        """ Ctor """
+        self.topology = TopologyInput()
+        pass
 
-    #     Args:
-    #     participant (str): The participant name.
-    #     mesh_id (str): The mesh ID.
-    #     data_id (str): The data ID.
-    #     coupling_scheme (str): The coupling scheme.
-    #     """
-    #     self.participant = participant
-    #     self.mesh_id = mesh_id
-    #     self.data_id = data_id
-    #     self.coupling_scheme = coupling_scheme
-
-    # def write_precice_xml_config(self, target: str) -> None:
-    #     """
-    #     Writes the preCICE configuration to the given target file.
-
-    #     Args:
-    #     target (str): The target file path.
-    #     """
-    #     root = etree.Element("precice-configuration")
-    #     interface = etree.SubElement(root, "interface")
-    #     participant = etree.SubElement(interface, "participant", name=self.participant)
-    #     mesh = etree.SubElement(participant, "mesh", id=self.mesh_id)
-    #     data = etree.SubElement(mesh, "data", id=self.data_id)
-    #     coupling_scheme = etree.SubElement(data, "coupling-scheme", type=self.coupling_scheme)
-    #     with open(target, "w") as config_file:
-    #         config_file.write(etree.tostring(root, encoding="unicode"))
-
-
-    def write_precice_xml_config(self, topology:TopologyInput, filename:str, log:UT_PCErrorLogging):
+    def write_precice_xml_config(self, filename:str, log:UT_PCErrorLogging):
         """ This is the main entry point to write preCICE config into an XML file"""
         # create the root element
         root = etree.Element("precice-configuration")
+
+        topology = self.topology
 
         for data, data_type in topology.data_from_exchanges:
             mystr = "scalar"
