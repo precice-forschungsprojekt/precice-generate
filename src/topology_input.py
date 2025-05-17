7ea7383e350cc490ec824fe4bae19e7654e8ca76
Toddelismyname <116207910+Toddelismyname@users.noreply.github.com>
2025-05-17 15:10:53 +0200

    v5

--- src/topology_input.py
+++ src/topology_input.py
@@ -8,6 +8,7 @@ class TopologyInput:
 
     def __init__(self):
         self.participants = {}
+        self.coupling_type = None
 
     def create_etree(self, etree, mylog: UT_PCErrorLogging) -> dict:
         """Create a etreeuration dictionary from the topology YAML file."""
@@ -59,3 +60,23 @@ class TopologyInput:
                 break
 
             # self.participants[new_participant.name] = new_participant
+
+        ##Exchanges
+        exchange_types = [exchange.get('type') for exchange in exchanges if 'type' in exchange]
+        if exchange_types:
+            # If all types are the same, set that as the coupling type
+            if len(set(exchange_types)) == 1:
+                if exchange_types[0] == 'strong' or exchange_types[0] == 'weak':
+                    self.coupling_type = exchange_types[0]
+                else:
+                    mylog.rep_error(f"Invalid exchange type: {exchange_types[0]}. Must be 'strong' or 'weak'.")
+                    self.coupling_type = None
+            else:
+                # Mixed types, default to weak
+                #mylog.rep_error("Mixed exchange types detected. Defaulting to 'weak'.")
+                self.coupling_type = 'weak'
+        #Convert to implicit or explicit
+        if self.coupling_type == 'strong':
+            self.coupling_type = 'implicit'
+        elif self.coupling_type == 'weak':
+            self.coupling_type = 'explicit'
