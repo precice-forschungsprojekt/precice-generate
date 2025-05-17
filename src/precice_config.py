5ec97931fdcea7e284953e9864bc12d5b5e231a7
Toddelismyname <116207910+Toddelismyname@users.noreply.github.com>
2025-05-17 17:06:16 +0200

    Revert "temp"
    This reverts commit 5a8990a2383d7b5941897076749036c84bc6aead.

--- src/precice_config.py
+++ src/precice_config.py
@@ -9,32 +9,7 @@ class PreciceConfig:
         """ Ctor """
         self.topology = TopologyInput()
         self.meshes = {}
-        self.meshes_to_receive = {}
         pass
-
-    def determine_mesh_reception(self):
-        """ Identify which meshes each participant needs to receive """
-        for exchange in self.topology.exchanges:
-            receiver = exchange["to"]
-            sender = exchange["from"]
-            receiver_patch = exchange["to-patch"]
-            sender_patch = exchange["from-patch"]
-            
-            # Ensure receiver gets the correct mesh from sender
-            if receiver not in self.meshes_to_receive:
-                self.meshes_to_receive[receiver] = {}
-            
-            self.meshes_to_receive[receiver][receiver_patch] = {
-                "from": sender,
-                "from_patch": sender_patch
-            }
-
-    def print_mesh_reception(self):
-        """ Display mesh reception logic """
-        for receiver, patches in self.meshes_to_receive.items():
-            print(f"Participant {receiver} receives meshes at:")
-            for patch, details in patches.items():
-                print(f"  - Patch {patch} receives from {details['from']} (patch {details['from_patch']})")
 
     def init_participants(self):
         """ Initialize participants """
